807e4672b4abead6cb93b23ec6eaac0d46b610b7
Seeba184 <seenasimkani@gmail.com>
2024-05-19 08:21:39 -0400

    Add method for finding legislators by zipcode

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,7 +1,26 @@
 require 'csv'
+require 'google/apis/civicinfo_v2'
 
 def clean_zip_code(zipcode)
   zipcode.to_s.rjust(5, '0')[0..4]
+end
+
+def legislators_by_zipcode(zipcode)
+  civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
+  civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
+
+  begin
+    legislators = civic_info.representative_info_by_address(
+      address: zipcode,
+      levels: 'country',
+      roles: %w[legislatorUpperBody legislatorLowerBody]
+    )
+    legislators = legislators.officials
+    legislators_names = legislators.map(&:name)
+    legislators_names.join(', ')
+  rescue StandardError
+    'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
+  end
 end
 
 puts 'EventManager initialized.'
@@ -16,5 +35,7 @@ contents.each do |row|
   name = row[:first_name]
   zipcode = clean_zip_code(row[:zipcode])
 
-  puts "#{name} #{zipcode}"
+  legislators = legislators_by_zipcode(zipcode)
+
+  puts "\n#{name} #{zipcode} #{legislators}\n"
 end
