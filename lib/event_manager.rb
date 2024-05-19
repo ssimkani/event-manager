ffcf6875d91ea98283f0ddf27ef87dee3f077c18
Seeba184 <seenasimkani@gmail.com>
2024-05-19 16:26:16 -0400

    Add ERB object template letter

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,5 +1,6 @@
 require 'csv'
 require 'google/apis/civicinfo_v2'
+require 'erb'
 
 def clean_zip_code(zipcode)
   zipcode.to_s.rjust(5, '0')[0..4]
@@ -8,16 +9,12 @@ end
 def legislators_by_zipcode(zipcode)
   civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
   civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
-
   begin
-    legislators = civic_info.representative_info_by_address(
+    civic_info.representative_info_by_address(
       address: zipcode,
       levels: 'country',
       roles: %w[legislatorUpperBody legislatorLowerBody]
-    )
-    legislators = legislators.officials
-    legislators_names = legislators.map(&:name)
-    legislators_names.join(', ')
+    ).officials
   rescue StandardError
     'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
   end
@@ -31,11 +28,16 @@ contents = CSV.open(
   header_converters: :symbol
 )
 
+template_letter = File.read('form_letter.erb')
+erb_template = ERB.new template_letter
+
 contents.each do |row|
   name = row[:first_name]
   zipcode = clean_zip_code(row[:zipcode])
 
   legislators = legislators_by_zipcode(zipcode)
 
-  puts "\n#{name} #{zipcode} #{legislators}\n"
+  form_letter = erb_template.result(binding)
+
+  puts form_letter
 end
